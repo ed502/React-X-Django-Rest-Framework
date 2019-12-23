a33ed996ccee0052b850e59c7119b7282a20a5c1
Chocobiv <47735101+Chocobiv@users.noreply.github.com>
2019-12-23 20:10:52 +0900

    소개 및 커리큘럼
    소개 및 커리큘럼 추가

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -31,7 +31,30 @@ function App() {
     <div>
     <img src="./intro6.jpg"></img>
     </div>
-
+    <header className="App-header">
+    <center>
+    <br/>
+      <h1>HACK YOUR LIFE</h1><br/>
+      <h2>내 아이디어를 내 손으로 실현한다</h2><br/><br/>
+      </center>
+      <p>
+        클래스 라이언을 통한 온라인 강의와 오프라인 멘토링 활동을 통한 프로그래밍 교육<br/><br/>
+        2018년부터 한국산업기술대학교에서 멋쟁이사자처럼 활동이 시작되었으며 지금까지 활발히 진행중입니다. <br/><br/>
+        과제 수행과 다양한 오프라인 활동을 통해 자신이 만들고자 하는 서비스를 직접 프로그래밍하여 만드는 것이 목표입니다. 
+      </p><br/><br/>
+      <center>
+      <div>
+        <img src="./0.jpg"></img>
+        </div>
+        <div>
+        <img src="./1.jpg"></img>
+        </div>
+        <div>
+        <img src="./2.jpg"></img>
+        </div>
+        </center>
+      </header>
+      
     </div>
   );
 }
